68586e831932bd9b7e0c084b161d82f742f4360f
dexz666 <142621320+dexz666@users.noreply.github.com>
2026-01-05 05:09:06 +0000

    Refactored code into functions for better scalability

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-name = input('Enter your name: ')
-print(f'Hello, {name}!')
+def greet(name="World"):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ") or "World"
+    greet(user_name)
